0a16d30cd0c3cfbe7ae6f8082e592bb351e16004
bright12037 <159770435+bright12037@users.noreply.github.com>
2024-05-28 10:58:30 +0100

    n

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -59,111 +59,111 @@ const Navbar = ({theColorFunc, navBarcolor}) => {
   return (
     
     <div className={navBarcolor === true ? 'MyNavBar lightmode' : 'MyNavBar darkmode'}>
-        <div className='TopNav'>
-            <p>Welcome to our Organic store</p>
-            <ul>
-                <li> <CiMail /> Grossshop@gmail.com</li>
-                <li id='NYC'> <VscLocation /> Washington, New york City</li>
-                <li onClick={usdShowDrop}><Image className='flag' src={flag} alt="flag" width={20}  /> English<FaCaretDown style={{fontSize:"15px"}} /></li>
-                <li id='usd' onClick={currencyShowDrop}>USD<FaCaretDown /></li>
-                <div onClick={theColorFunc}>
-                        {
-                            navBarcolor === true ?
-                            <li>Dark <IoMdMoon /></li> :
-                            <li>Light <IoSunnySharp /></li>
-                        }
+            <div className='TopNav'>
+                <p>Welcome to our Organic store</p>
+                <ul>
+                    <li> <CiMail /> Grossshop@gmail.com</li>
+                    <li id='NYC'> <VscLocation /> Washington, New york City</li>
+                    <li onClick={usdShowDrop}><Image className='flag' src={flag} alt="flag" width={20}  /> English<FaCaretDown style={{fontSize:"15px"}} /></li>
+                    <li id='usd' onClick={currencyShowDrop}>USD<FaCaretDown /></li>
+                    <div onClick={theColorFunc}>
+                            {
+                                navBarcolor === true ?
+                                <li>Dark <IoMdMoon /></li> :
+                                <li>Light <IoSunnySharp /></li>
+                            }
+                        </div>
+                </ul>
+            </div>
+
+            <div className='parentBottomnav'>
+                    
+                    <div className= {navBarcolor === true ? 'BottomNav' :'BottomNav BottomNav2'} >
+                            <Image src = {logo} alt = "a logo" width={200} />
+                            <ul>
+                                <li onClick={buttonShow} id='browse'>Browse Category<VscChevronDown /></li>
+
+                                <li id="home">Home<VscChevronDown /></li>
+                                <li>Products<VscChevronDown /></li>
+                                <li>Blog<VscChevronDown /></li>
+                                <li>Pages<VscChevronDown /></li>
+                            </ul>
+
+                            <div className='BottomNavicons'>
+                                <p><CiSearch /></p>
+                                <p><CgProfile /></p>
+                                <p><IoBagOutline /></p>
+                                <p onClick={buttonShow1} id='DotsIcon'><TbGridDots /></p>
+                            </div> 
+                            <p onClick={buttonShow1} id='DotsIcon2'><TbGridDots /></p>
                     </div>
+            </div>
+
+            {Show === false ? ('') : ( 
+                <div className='DropdownBrowse' data-aos="fade-up" data-aos-duration="200">
+                <ul>
+                    <li> vegetables</li>
+                    <li>Baby Care</li>
+                    <li>vegetables</li>
+                    <li>Cleaning</li>
+                    <li>vegetables</li>        
+                </ul>
+            
+
+            <ul>
+                <li> Bakery and Biscuits</li>
+                <li>vegetables</li>
+                <li>Coffee and Drinks</li>
+                <li>vegetables</li>
+                <li>Beauty and Health</li>
             </ul>
-        </div>
 
-
-          <div className='parentBottomnav'>
-                
-                <div className= {navBarcolor === true ? 'BottomNav' :'BottomNav BottomNav2'} >
-                        <Image src = {logo} alt = "a logo" width={200} />
-                        <ul>
-                            <li onClick={buttonShow} id='browse'>Browse Category<VscChevronDown /></li>
-
-                            <li id="home">Home<VscChevronDown /></li>
-                            <li>Products<VscChevronDown /></li>
-                            <li>Blog<VscChevronDown /></li>
-                            <li>Pages<VscChevronDown /></li>
-                        </ul>
-
-                        <div className='BottomNavicons'>
-                            <p><CiSearch /></p>
-                            <p><CgProfile /></p>
-                            <p><IoBagOutline /></p>
-                            <p onClick={buttonShow1} id='DotsIcon'><TbGridDots /></p>
-                        </div> 
-                        <p onClick={buttonShow1} id='DotsIcon2'><TbGridDots /></p>
-                </div>
-          </div>
-        {Show === false ? ('') : ( 
-             <div className='DropdownBrowse' data-aos="fade-up" data-aos-duration="200">
-             <ul>
-                 <li> vegetables</li>
-                 <li>Baby Care</li>
-                 <li>vegetables</li>
-                 <li>Cleaning</li>
-                 <li>vegetables</li>        
-             </ul>
+            <ul>
+                <li>Jam and Jellyvegetables</li>
+                <li>Sports and Fitness</li>
+                <li>Cold Drinks</li>
+                <li>Fresh and Organic</li>
+                <li>Honey</li>
+            </ul>
+            </div>)}
          
-
-         <ul>
-             <li> Bakery and Biscuits</li>
-             <li>vegetables</li>
-             <li>Coffee and Drinks</li>
-             <li>vegetables</li>
-             <li>Beauty and Health</li>
-         </ul>
-
-         <ul>
-             <li>Jam and Jellyvegetables</li>
-             <li>Sports and Fitness</li>
-             <li>Cold Drinks</li>
-             <li>Fresh and Organic</li>
-             <li>Honey</li>
-         </ul>
-     </div>)}
-         
-        {Show1 === false ? ('') : (
-            <div className='DropdownDot' data-aos="fade-up" data-aos-duration="1000">
-            <div style={{display:"flex", alignItems:"center"}}>
-                    <Image src={logo} alt='logo' width={200}/>
-                    <p onClick={buttonShow1} style={{marginLeft:"auto", fontSize:"20px", color:"rgb(255, 124, 10)", padding:"5px", borderRadius:"50%", backgroundColor:"rgb(247, 182, 128)", justifyContent:"center", display:"flex", cursor:"pointer"}}><IoCloseSharp /></p>
-            </div>
-           <h1>About Us</h1>
-            <p>Explain to you how all this mistaken <br /> denouncing pleasure and 
-                praising pain was <br /> born and we will give you a complete account <br /> of the
-                 system, and expound <br /> the actual teachings. <br/><br/>
-                 Mistaken denouncing pleasure and 
-                 praising pain was born and we <br /> will give you complete 
-                 account of the system <br /> expound.</p>
-            <button>About us</button>
-            <h1 >Contact info</h1>
-            <p>Chicago 12, Melborne City, <br /> USA +8801682648101 <br /> info@example.com</p>
-            <div>
-                <p><CiFacebook /></p>
-                <p><CiInstagram /></p>
-                <p><FaTwitter /></p>
-                <p><FaYoutube /></p>
-            </div>
-         </div>
-
-
-        )}
-
-                <div className='dropdown2'>
-                    <p> Grocery Store</p>
-                    <p> Grocery Store</p>
-                    <p> Grocery Store</p>
-                    <p> Grocery Store</p>
-                    <p> Grocery Store</p>
-                    <p> Grocery Store</p>
+            {Show1 === false ? ('') : (
+                <div className='DropdownDot' data-aos="fade-up" data-aos-duration="1000">
+                <div style={{display:"flex", alignItems:"center"}}>
+                        <Image src={logo} alt='logo' width={200}/>
+                        <p onClick={buttonShow1} style={{marginLeft:"auto", fontSize:"20px", color:"rgb(255, 124, 10)", padding:"5px", borderRadius:"50%", backgroundColor:"rgb(247, 182, 128)", justifyContent:"center", display:"flex", cursor:"pointer"}}><IoCloseSharp /></p>
                 </div>
+            <h1>About Us</h1>
+                <p>Explain to you how all this mistaken <br /> denouncing pleasure and 
+                    praising pain was <br /> born and we will give you a complete account <br /> of the
+                    system, and expound <br /> the actual teachings. <br/><br/>
+                    Mistaken denouncing pleasure and 
+                    praising pain was born and we <br /> will give you complete 
+                    account of the system <br /> expound.</p>
+                <button>About us</button>
+                <h1 >Contact info</h1>
+                <p>Chicago 12, Melborne City, <br /> USA +8801682648101 <br /> info@example.com</p>
+                <div>
+                    <p><CiFacebook /></p>
+                    <p><CiInstagram /></p>
+                    <p><FaTwitter /></p>
+                    <p><FaYoutube /></p>
+                </div>
+            </div>
 
-              { Show2 === false ? ('') : (
+
+            )}
+
+            <div className='dropdown2'>
+                    <p> Grocery Store</p>
+                    <p> Grocery Store</p>
+                    <p> Grocery Store</p>
+                    <p> Grocery Store</p>
+                    <p> Grocery Store</p>
+                    <p> Grocery Store</p>
+            </div>
+
+            { Show2 === false ? ('') : (
                   <div className='usddrop' data-aos="fade-up" data-aos-duration="200" >
                   <div style={{display: "flex", color: "black", gap:"10px", fontSize:"14px", alignItems:"center"}}>
                       <Image src={flag} alt="flag" width={20} />
@@ -178,13 +178,13 @@ const Navbar = ({theColorFunc, navBarcolor}) => {
                       <p>Cameroon</p>
                   </div>
               </div>
-              )}
+            )}
 
-              {Show3 === false ? ('') : (
+            {Show3 === false ? ('') : (
                 <div className='currency'data-aos="fade-up" data-aos-duration="200">
                     <p><TbCurrencyYuan />Chinese</p>
                     <p><FaYenSign />Japan</p>
-                </div>)}
+            </div>)}
     </div>
   )
 }
